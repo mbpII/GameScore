eeec85319c87b5740188e57e42c76952879233a6
Matthew Parks <108713717+mbpII@users.noreply.github.com>
2024-11-03 13:42:53 -0500

    updating assembly

--- obj/Debug/net8.0/GameScoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameScoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameScoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118e37f53f1c5ced8586127a666fbebda2afc782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632a1174a666c4be5e5505668d084dcb3dfcdee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameScoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameScoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
